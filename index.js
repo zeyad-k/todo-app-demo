8440c3f684602d3595a582d544a8cfa2b4dfefff
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-03-02 13:51:58 +0200

    add dark theme functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log('hi hoooda');
+
+const DarkThemeToggleElement = document.querySelector('.DarkThemeToggle')
+const AppElement = document.querySelector('.App')
+
+const ToggleDarkTheme = () => {
+	console.log('hi hooooda , you will turn the theme');
+	AppElement.classList.toggle('App--isDark')
+}
+
+DarkThemeToggleElement.addEventListener('click', (event) => {
+	event.preventDefault();
+	ToggleDarkTheme();
+
+})
