acf5068260e2175a1162e713b48d7c45ce58fe35
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-03-02 14:11:51 +0200

    Add ESLint and update Dark Theme toggle

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// console.log('hi hoooda');
-
-const DarkThemeToggleElement = document.querySelector('.DarkThemeToggle')
-const AppElement = document.querySelector('.App')
+const DarkThemeToggleElement = document.querySelector('.DarkThemeToggle');
+const AppElement = document.querySelector('.App');
 
 const ToggleDarkTheme = () => {
-	console.log('hi hooooda , you will turn the theme');
-	AppElement.classList.toggle('App--isDark')
-}
+  console.log('hi hooooda , you will turn the theme');
+  AppElement.classList.toggle('App--isDark');
+};
 
 DarkThemeToggleElement.addEventListener('click', (event) => {
-	event.preventDefault();
-	ToggleDarkTheme();
-
-})
+  event.preventDefault();
+  ToggleDarkTheme();
+});
