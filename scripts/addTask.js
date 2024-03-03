e46ff4bd80262b5c3256a29f09bac1745a46f756
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-03-04 01:23:19 +0200

    Remove commented out code in addTask.js

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -12,16 +12,6 @@ const addTask = () => {
     isCompleted: false,
   };
   const taskList = fetchData('tasks') || [];
-  //     taskListElement.innerHTML += `<li class="TaskList__taskContent" >
-  // <div class="TaskList__checkbox" tabindex='0' role='button' >
-  // <img class="TaskList__checkboxImg" scr="./assets/icon-checkmark.svg" >
-  // </div>
-  // <div class="TaskList__valueContent">
-  // <p class="TaskList__value">${task}</p>
-  // <img class="TaskList__deleteIcon" src="./assets/icon-basket.svg" alt="basket-icon">
-  // </div>
-  // </li>`;
-  //   });
 
   taskList.push(task);
   saveToDatabase('tasks', taskList);
